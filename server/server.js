204fd9622f17b8fab49541c6903b0ba6bc7552b1
Hecan7 <7hecano@gmail.com>
2022-01-11 01:27:42 -0800

    Added a third project

--- server/server.js
+++ server/server.js
@@ -15,4 +15,11 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+}
+
 app.use(router);
