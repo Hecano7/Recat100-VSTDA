ca36f7fadfe72bc401ecdf768f0a2162269cc778
Hecan7 <7hecano@gmail.com>
2022-01-10 22:20:47 -0800

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -7,4 +7,19 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
+const path = require('path');
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log('listening at http://localhost:4000');
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
