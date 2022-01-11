468ba383b6542bb059927571e966a196eb3c9e98
Hecan7 <7hecano@gmail.com>
2022-01-10 23:49:47 -0800

    deploy to heroku second try

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const server = require('./server');
+// const server = require('./server');
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+// server.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
+var PORT = process.env.PORT || 5000;
 const express = require('express');
-
+const morgan = require('morgan');
+const router = express.Router();
+const bodyParser = require('body-parser');
 const app = express();
 
-const path = require('path');
+app.listen(PORT, () => {
+  console.log('listening at http://localhost:5000');
+});
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.set('views' , './views');
+
+// app.set('view engine' , 'ejs');
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
 
-module.exports = app;
+
+app.use(router);
+
