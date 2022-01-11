b62b671897bbefc8be61058c67a8a3fa6624322b
Hecan7 <7hecano@gmail.com>
2022-01-11 00:44:02 -0800

    Added a third project

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const morgan = require('morgan');
 const router = express.Router();
-const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(PORT, () => {
@@ -11,13 +10,9 @@ app.listen(PORT, () => {
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname });
 });
 
-
 app.use(router);
-
